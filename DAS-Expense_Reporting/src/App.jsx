7c663bbd838ac503f715c98392642f5a36519344
F15Woodley <rwoodley@digitalaerial.com>
2026-05-21 10:29:20 -0400

    Update App.jsx

--- DAS-Expense_Reporting/src/App.jsx
+++ DAS-Expense_Reporting/src/App.jsx
@@ -2,7 +2,10 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { expenseService } from "./expenseService";
 import { supabase } from "./supabaseClient";
 import * as pdfjsLib from "pdfjs-dist";
-pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker?url";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
 async function renderPdfToImages(pdfUrl) {
   const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
   const images = [];
